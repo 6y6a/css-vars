cc84404373bfd1baa8a316bc702b9e22efea7bb2
6y6a <gubinalexeymain@gmail.com>
2019-10-29 23:28:38 +0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './index.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="box">
+                    <h1> Css is awersome</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
