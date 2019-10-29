46543fc0a7cd5e95446c8ee9caa710b0b421732e
6y6a <gubinalexeymain@gmail.com>
2019-10-29 23:46:37 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,25 @@ import React, {Component} from 'react';
 import './index.css'
 
 class App extends Component {
+    handleDarkMode = () => {
+        window.document.body.setAttribute('data-theme', 'dark')
+    }
+
+    handleLightMode = () => {
+        window.document.body.setAttribute('data-theme', 'default')
+    }
+
     render() {
         return (
-            <div>
+            <div className='container'>
                 <div className="box">
                     <h1> Css is awersome</h1>
+                </div>
+
+
+                <div className="pane">
+                    <button onClick={this.handleDarkMode}>Темная тема</button>
+                    <button onClick={this.handleLightMode}>Светлая тема</button>
                 </div>
             </div>
         );
